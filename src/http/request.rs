125abed91e81422d488e656a53d2c8fab637ffa0
antoniomatiolijr <antoniomatiolijr@gmail.com>
2021-02-05 08:47:44 -0300

    feat(method): add parsing method

--- src/http/request.rs
+++ src/http/request.rs
@@ -1,4 +1,4 @@
-use super::method::Method;
+use super::method::{Method, MethodError};
 use std::convert::TryFrom;
 use std::error::Error;
 use std::fmt::{Debug, Display, Formatter, Result as FmtResult};
@@ -16,16 +16,23 @@ impl TryFrom<&[u8]> for Request {
     fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
         let request = str::from_utf8(buf)?;
         let (method, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
-        let (path, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+        let (mut path, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
         let (protocol, _) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
 
         if protocol != "HTTP/1.1" {
-          return Err(ParseError::InvalidProtocol);
+            return Err(ParseError::InvalidProtocol);
+        }
+
+        let method: Method = method.parse()?;
+
+        let mut query_string = None;
+        if let Some(i) = path.find("?")  {
+          query_string = Some(&path[i + 1..]);
+          path = &path[..i];
         }
 
         unimplemented!()
-
-      }
+    }
 }
 
 fn get_next_word(request: &str) -> Option<(&str, &str)> {
@@ -54,6 +61,12 @@ impl ParseError {
             Self::InvalidMethod => "Invalid Method",
         }
     }
+}
+
+impl From<MethodError> for ParseError {
+  fn from(_: MethodError) -> Self {
+      Self::InvalidMethod
+  }
 }
 
 impl From<Utf8Error> for ParseError {
