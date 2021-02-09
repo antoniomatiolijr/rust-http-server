d3cb09e094621e0fe67a1ba65464370564600f10
antoniomatiolijr <antoniomatiolijr@gmail.com>
2021-02-08 23:51:41 -0300

    implement query string parser

--- src/server.rs
+++ src/server.rs
@@ -24,7 +24,9 @@ impl Server {
                         Ok(_) => {
                             println!("Received a request: {}", String::from_utf8_lossy(&buffer));
                             match Request::try_from(&buffer[..]) {
-                                Ok(_request) => {}
+                                Ok(request) => {
+                                  dbg!(request);
+                                }
                                 Err(err) => println!("Failed to parse a request: {}", err),
                             }
                         }
